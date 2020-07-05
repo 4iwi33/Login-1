f7dd967d92c7076d1531009c7d6634e09651edb8
zhaneuski <59090092+zhaneuski@users.noreply.github.com>
2020-07-05 18:13:16 +0300

    123

---
+++ core/config.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+class Config
+{
+    const MYSQL_HOST = 'localhost';
+    const MYSQL_USER_NAME = 'root';
+    const MYSQL_PASSWORD = 'root';
+    const MYSQL_DATABASE = 'users';
+    const PAGE_SIZE = 3;
+    const MYSQL_TABLE = 'login';
+}
