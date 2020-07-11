751c2f94d981351f538d62afcd7ac7bb137e4635
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-07-11 16:28:01 +0300

    Update config.php

--- core/config.php
+++ core/config.php
@@ -6,8 +6,8 @@ class Config
 {
     const MYSQL_HOST = 'localhost';
     const MYSQL_USER_NAME = 'root';
-    const MYSQL_PASSWORD = '';
-    const MYSQL_DATABASE = 'users';
+    const MYSQL_PASSWORD = 'root';
+    const MYSQL_DATABASE = 'log';
     const PAGE_SIZE = 3;
-    const MYSQL_TABLE = 'login';
+    const MYSQL_TABLE = 'form';
 }
